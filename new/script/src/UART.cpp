dbef6154a0562ea48868b5692a0bf14112a4f56e
Mafkees35 <34511154+Mafkees35@users.noreply.github.com>
2018-05-09 11:05:08 +0200

    UART UPDATE

--- new/script/src/UART.cpp
+++ new/script/src/UART.cpp
@@ -18,4 +18,44 @@ UART::~UART() {
 	// TODO Auto-generated destructor stub
 }
 
+
+/*!
+ * \brief teken een lijn.
+ * \param paramter int.
+ */
+int UART::write(char *text_out){
+
+}
+
+/*!
+ * \brief teken een lijn.
+ * \param paramter int.
+ */
+int UART::read(char *buf){
+
+}
+
+/*!
+ * \brief teken een lijn.
+ * \param paramter int.
+ */
+int UART::init(){
+
+}
+
+/*!
+ * \brief teken een lijn.
+ * \param paramter int.
+ */
+int UART::putchar(char c){
+
+}
+
+/*!
+ * \brief de VGA class
+ */
+int UART::getchar(char c){
+
+}
+
 } /* namespace UART */
