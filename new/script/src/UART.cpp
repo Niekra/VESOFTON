19f49c1fd538607c0db6f91c159aa69ed2755412
Mafkees35 <34511154+Mafkees35@users.noreply.github.com>
2018-05-09 10:44:46 +0200

    UART.h/ .cpp

---
+++ new/script/src/UART.cpp
@@ -0,0 +1,21 @@
+/*
+ * UART.cpp
+ *
+ *  Created on: May 9, 2018
+ *      Author: M
+ */
+
+#include <UART.h>
+
+namespace UART {
+
+UART::UART() {
+	// TODO Auto-generated constructor stub
+
+}
+
+UART::~UART() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace UART */
--- new/script/src/Vgascreen.cpp
+++ new/script/src/Vgascreen.cpp
@@ -23,11 +23,13 @@ Vgascreen::~Vgascreen() {
 int Vgascreen::init_VGA(){
 	UB_VGA_Screen_Init(); // Init VGA-Screen
 	UB_VGA_FillScreen(VGA_COL_GREEN);// Set screen green.
+	return 0;
 }
 
 int Vgascreen::VGA_pos(int x, int y){
 	x_pos = x;// Set x position.
 	y_pos = y;// Set y position.
+	return 0;
 }
 
 int Vgascreen::draw_line(int x1, int y1, int x2, int y2, int width, int color){
@@ -82,6 +84,10 @@ int Vgascreen::draw_line(int x1, int y1, int x2, int y2, int width, int color){
 	return 0;
 }
 
+int Vgascreen::draw_ellipse(int x_mp, int y_mp, int x_rad, int y_rad,int color){
+	return 0;
+}
+
 int Vgascreen::draw_rectangle(int x_lo, int y_lo, int x_rb, int y_rb, int color){
 	  draw_line(x_lo,y_rb,x_rb,y_rb,1,254);
 	  draw_line(x_rb,y_rb+1,x_rb,y_lo,1,254);
@@ -93,4 +99,12 @@ int Vgascreen::draw_triangle(int x1, int y1, int x2, int y2, int x3, int y3, int
 	  draw_line(x1,y1,x2,y2,1,254);
 	  draw_line(x1,y1,x3,y3,1,254);
 	  draw_line(x2,y2,x3,y3,1,254);
+}
+
+int Vgascreen::draw_text(int x, int y, char *text, char* font_name, int color, int style){
+	return 0;
+}
+
+int Vgascreen::draw_bitmap(int nr, int x_lo, int y_lo){
+	return 0;
 }
